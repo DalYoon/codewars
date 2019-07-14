7654b1b94f7190a906ed1eb3e9c43e029bbc57da
Dal Yoon <yeodal89@gmail.com>
2019-07-14 22:59:00 +0900

    add comment

--- practice/7kyu/Isograms.js
+++ practice/7kyu/Isograms.js
@@ -1,8 +1,8 @@
 const isIsogram = str => {
   return !str
-    .toLowerCase()
-    .split("")
-    .some((letter, index, self) => self.indexOf(letter) !== self.lastIndexOf(letter));
+    .toLowerCase() // to lower case
+    .split("") // string to array
+    .some((letter, index, self) => self.indexOf(letter) !== self.lastIndexOf(letter)); // repeating check
 };
 
 export default isIsogram;
