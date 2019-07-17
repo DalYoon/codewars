d65c80865ac166b8ee12b2fb6a2634d4c87f1e9a
Dal Yoon <yeodal89@gmail.com>
2019-07-18 00:16:53 +0900

    20190717

---
+++ practice/5kyu/Where_my_anagrams_at.js
@@ -0,0 +1,35 @@
+// yeah i'm fuckin idiot
+
+const anagrams = (word, words) => {
+  const wordCounts = getSpellCounts(word);
+  const wordsCounts = words.map((eachWord, index) => ({ index, counts: getSpellCounts(eachWord) }));
+  const matched = [];
+
+  wordsCounts.forEach(
+    ({ index, counts }) => compareObject(wordCounts, counts) && matched.push(words[index])
+  );
+
+  return matched;
+};
+
+const getSpellCounts = word => {
+  const unordered = {};
+  const ordered = {};
+
+  word
+    .split("")
+    .forEach(letter => (unordered[letter] ? (unordered[letter] += 1) : (unordered[letter] = 1)));
+
+  Object.keys(unordered)
+    .sort()
+    .forEach(key => (ordered[key] = unordered[key]));
+
+  return ordered;
+};
+
+const compareObject = (obj1, obj2) => {
+  const targetLoop = Object.keys(obj2).length > Object.keys(obj1).length ? obj2 : obj1;
+  return !Object.keys(targetLoop).some(key => obj1[key] !== obj2[key]);
+};
+
+export default anagrams;
